281b0ed3775b98db8ce38abb6422ae323e5d55bd
JonasSan <jonas.sandal@hotmail.de>
2019-01-11 14:16:42 +0100

    Made PostMapping work

--- src/main/java/com/SundayScaries/MurderClueWalk/controller/UserController.java
+++ src/main/java/com/SundayScaries/MurderClueWalk/controller/UserController.java
@@ -5,10 +5,13 @@ import com.SundayScaries.MurderClueWalk.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+@Controller
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -29,7 +32,7 @@ public class UserController {
         redirectAttributes
                 .addAttribute(user10)
                 .addFlashAttribute("success", true);
-        return "index";
+        return "profile";
     }
 
 
